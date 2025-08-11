18a1d6d24c5a58cbaa347e6b775592f2066c4b9a
Kaushik <kaushik.s.saravanan@gmail.com>
2025-08-11 09:42:49 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,11 +59,11 @@ export default function Page() {
         altText={work.company}
         title={work.company}
         subtitle={work.title}
-        // href={work.href}
-        // badges={work.badges}
+        href={work.href || undefined}
+        badges={work.badges || undefined}
         period={`${work.start} - ${work.end ?? "Present"}`}
         description={work.description}
-        linkedinPosts={work.linkedinPosts}
+        linkedinPosts={work.linkedinPosts || undefined}
       />
     ))}
   </div>
@@ -132,15 +132,15 @@ export default function Page() {
                 delay={BLUR_FADE_DELAY * 12 + id * 0.05}
               >
                 <ProjectCard
-                  href={project.href}
+                  href={project.href || undefined}
                   key={project.title}
                   title={project.title}
                   description={project.description}
                   dates={project.dates}
                   tags={project.technologies}
                   image={project.image}
-                  video={project.video}
-                  links={project.links}
+                  video={project.video || undefined}
+                  links={project.links || undefined}
                 />
               </BlurFade>
             ))}
@@ -182,7 +182,7 @@ export default function Page() {
                     location={project.location}
                     dates={project.dates}
                     image={project.image}
-                    links={project.links}
+                    links={project.links || undefined}
                   />
                 </BlurFade>
               ))}
